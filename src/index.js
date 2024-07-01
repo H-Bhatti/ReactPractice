3492d53d3fd3c339e5460855a834605e94e7d12d
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2024-07-01 20:46:20 +0400

    learned about image attriubtes and added images and link

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "Hammad Bhatti";
+const randomImage1 = "https://picsum.photos/200/200";
+const randomImage2 = "https://picsum.photos/300/300";
+const randomImage3 = "https://picsum.photos/200/300";
+const link = "https://github.com/H-Bhatti";
 
 const currentDate = new Date();
 console.log(currentDate.toLocaleString());
 ReactDOM.render(
   <>
-    <h1>My naame is {name} </h1>
+    <h1 contentEditable="true">My naame is {name} click on the third image</h1>
     <p>Date is:: {currentDate.toLocaleDateString()}</p>
     <p>Time is : {currentDate.toLocaleTimeString()}</p>
+    <img src={randomImage1} alt="random Image" />
+    <img src={randomImage2} alt="random Image" />
+    <a href={link} target="_newTab">
+      <img src={randomImage3} alt="random Image" />
+    </a>
   </>,
   document.getElementById("root")
 );
